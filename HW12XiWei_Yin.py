d1456981de5aa8199e969d728c82767d5a853cc2
Raymond Yin <raymondxiwei@gmail.com>
2018-12-11 18:41:34 -0500

    Main Flask logic and Jinja2 template files added

--- HW12XiWei_Yin.py
+++ HW12XiWei_Yin.py
@@ -9,13 +9,28 @@ url:
 https://sit.instructure.com/courses/28343/assignments/118688
 """
 
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "hello Flask!"
+
+@app.route('/instructor_courses')
+def instructor_courses():
+    DB_FILE = "810_startup.db"
+
+    query = """SELECT i.CWID, i.Name, i.Dept, g.Course, count(*) AS Students FROM Instructors i 
+                JOIN Grades g on i.CWID=g.Instructor_CWID GROUP BY g.Course order by i.Name;"""
+    db = sqlite3.connect(DB_FILE)
+    rows = db.execute(query)
+
+    data = [{'CWID': cwid, 'Name': name, 'Department': department, 'Courses': courses, 'Students': students}
+            for cwid, name, department, courses, students in rows]
+
+    db.close()
+
+    return render_template('instructor_courses.html',
+                           instructors=data)
 
 
 if __name__ == '__main__':
